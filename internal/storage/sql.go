c2fa2eb0f95efc12ea06457c99e020a182b81671
Rich <11451714+Rich7690@users.noreply.github.com>
2020-02-01 17:15:53 -0800

    Update sql.go

--- internal/storage/sql.go
+++ internal/storage/sql.go
@@ -8,7 +8,7 @@ import (
 import _ "github.com/jinzhu/gorm/dialects/sqlite"
 
 func MigrateDB() {
-	log.Info().Msg("Migrating DB")
+	/*log.Info().Msg("Migrating DB")
 	db, err := gorm.Open("sqlite3", "test.db")
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to connect database")
@@ -16,5 +16,5 @@ func MigrateDB() {
 	defer db.Close()
 
 	// Migrate the schema
-	db.AutoMigrate()
+	db.AutoMigrate()*/
 }
